1948a5238aaecaa476c92507c3a7cf6221460556
Patrick Stahl <p.stahl@shopware.com>
2019-03-26 07:46:41 +0100

    Initial Commit

---
+++ AdministrationNewField.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace AdministrationNewField;
+
+use Shopware\Core\Framework\Plugin;
+
+class AdministrationNewField extends Plugin
+{
+}
---
+++ Resources/views/administration/main.js
@@ -0,0 +1,6 @@
+import { Component } from 'src/core/shopware';
+import template from './src/extension/sw-product-settings-form/sw-product-settings-form.html.twig';
+
+Component.override('sw-product-settings-form', {
+    template,
+});
