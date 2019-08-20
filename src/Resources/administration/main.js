bae7ad643a0cbe42233e90155f060560d4a8a9c5
Stephan Pohl <stp@shopware.com>
2019-08-20 13:18:40 +0200

    Use third party interface

--- src/Resources/administration/main.js
+++ src/Resources/administration/main.js
@@ -1,5 +1,6 @@
-import { Component } from 'src/core/shopware';
 import template from './src/extension/sw-product-settings-form/sw-product-settings-form.html.twig';
+
+const { Component } = Shopware;
 
 Component.override('sw-product-settings-form', {
     template
